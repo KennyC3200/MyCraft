79d0d4a3dfdcf57030583c7509d837270874f69c
Kenny Chen <kennychen3200@gmail.com>
2024-12-22 20:12:19 -0500

    Finished Mouse class I think

--- app/src/main/java/MyCraft/input/Mouse.java
+++ app/src/main/java/MyCraft/input/Mouse.java
@@ -4,17 +4,64 @@ import MyCraft.gfx.*;
 
 import org.joml.*;
 
+import static org.lwjgl.glfw.GLFW.*;
+import static org.lwjgl.opengl.GL33.*;
+
 public class Mouse {
 
-    private Window;
+    private Window window;
 
-    private Vector2f position, positionDelta;
+    private Vector2d position, positionDelta;
+    private Button[] keys;
+    private boolean toggled;
+    private boolean firstMove;
 
+    /* Init the mouse */
     public Mouse(Window window) {
         this.window = window;
+        this.position = new Vector2d(0, 0);
+        this.positionDelta = new Vector2d(0, 0);
+        this.keys = new Button[GLFW_MOUSE_BUTTON_LAST];
+        this.toggled = true;
+        this.firstMove = true;
     }
 
+    /* Update the mosue */
     public void update() {
+        double[] x = new double[1];
+        double[] y = new double[1];
+        glfwGetCursorPos(window.getHandle(), x, y);
+
+        if (firstMove) {
+            position.x = x[0];
+            position.y = y[0];
+            firstMove = false;
+        }
+
+        positionDelta.x = x[0] - position.x;
+        positionDelta.y = y[0] - position.y;
+
+        position.x = x[0];
+        position.y = y[0];
+
+        for (int i = GLFW_MOUSE_BUTTON_1; i < GLFW_MOUSE_BUTTON_LAST; i++) {
+            int state = glfwGetMouseButton(window.getHandle(), i);
+            if (state == GLFW_PRESS) {
+                keys[i].down = true;
+            }
+            if (state == GLFW_RELEASE) {
+                keys[i].down = false;
+            }
+
+            keys[i].pressed = keys[i].down && !keys[i].last;
+            keys[i].last = keys[i].down;
+        }
+
+        glfwSetInputMode(
+            window.getHandle(), 
+            GLFW_CURSOR, 
+            toggled ? GLFW_CURSOR_NORMAL : GLFW_CURSOR_DISABLED
+        );
     }
 
 }
